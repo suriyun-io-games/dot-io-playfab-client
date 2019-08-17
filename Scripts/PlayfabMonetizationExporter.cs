3531b9af8813ad8c7f8ec9e833c7ef01e621eeee
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-17 23:07:12 +0700

    WIP monetization exporter, now catalog exporter is work, but its structure may changes after worked on IAP

--- Scripts/PlayfabMonetizationExporter.cs
+++ Scripts/PlayfabMonetizationExporter.cs
@@ -5,38 +5,99 @@ using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+using LitJson;
 
 public class PlayfabMonetizationExporter : MonoBehaviour
 {
+    public GameInstance gameInstance;
     public MonetizationManager monetizationManager;
 
-#if UNITY_EDITOR
-    [ContextMenu("Export Products")]
-    public void ExportProducts()
-    {
+    [Header("Catalog")]
+    public string exportingCatalogVersion = "Catalog-1";
 
+#if UNITY_EDITOR
+    [ContextMenu("Export Catalog")]
+    public void ExportCatalog()
+    {
+        var allItems = gameInstance.GetAllItems();
+        var allBundles = monetizationManager.products;
+        var catalogItems = new List<CatalogItem>();
+        foreach (var item in allItems)
+        {
+            var virtualCurrencyPrices = new Dictionary<string, int>();
+            if (!string.IsNullOrEmpty(item.price.id))
+                virtualCurrencyPrices[item.price.id] = item.price.amount;
+            foreach (var price in item.prices)
+            {
+                if (!string.IsNullOrEmpty(price.id))
+                    virtualCurrencyPrices[price.id] = price.amount;
+            }
+            catalogItems.Add(new CatalogItem()
+            {
+                ItemId = item.GetId(),
+                CatalogVersion = exportingCatalogVersion,
+                DisplayName = item.GetTitle(),
+                Description = item.GetDescription(),
+                VirtualCurrencyPrices = virtualCurrencyPrices,
+                Consumable = new CatalogConsumable(),
+                Bundle = new CatalogBundle(),
+            });
+        }
+
+        foreach (var bundle in allBundles)
+        {
+            var virtualCurrencyPrices = new Dictionary<string, int>();
+            virtualCurrencyPrices.Add("RM", 0); // TODO: SET USD PRICE
+            var itemIds = new List<string>();
+            foreach (var item in bundle.items)
+            {
+                itemIds.Add(item.GetId());
+            }
+            var rewardCurrencies = new Dictionary<string, int>();
+            foreach (var currency in bundle.currencies)
+            {
+                rewardCurrencies[currency.id] = currency.amount;
+            }
+            catalogItems.Add(new CatalogItem()
+            {
+                ItemId = bundle.GetId(),
+                CatalogVersion = exportingCatalogVersion,
+                DisplayName = bundle.GetTitle(),
+                Description = bundle.GetDescription(),
+                VirtualCurrencyPrices = virtualCurrencyPrices,
+                Consumable = new CatalogConsumable(),
+                Bundle = new CatalogBundle()
+                {
+                    BundledItems = itemIds,
+                    BundledVirtualCurrencies = rewardCurrencies,
+                }
+            });
+        }
+
+        var dropTables = new List<DropTableItem>();
+
+        var catalog = new PlayfabCatalog()
+        {
+            CatalogVersion = exportingCatalogVersion,
+            Catalog = catalogItems,
+            DropTables = dropTables,
+        };
+        var json = JsonMapper.ToJson(catalog);
+        var path = EditorUtility.SaveFilePanel("Export Catalog", Application.dataPath, "CATALOG", "json");
+        if (path.Length > 0)
+            File.WriteAllText(path, json);
     }
 
     [ContextMenu("Export Currencies")]
     public void ExportCurrencies()
     {
-        var json = "[]";
-        var i = 0;
+        var currencies = new List<PlayfabCurrency>();
         foreach (var currency in monetizationManager.currencies)
         {
-            var code = currency.id.Substring(0, 2);
+            var code = currency.id;
             var name = currency.name;
             var startAmount = currency.startAmount;
-            if (i == 0)
-            {
-                json = "[";
-            }
-            else
-            {
-                json += ",";
-            }
-
-            json += JsonUtility.ToJson(new PlayfabCurrency()
+            currencies.Add(new PlayfabCurrency()
             {
                 CurrencyCode = code,
                 DisplayName = name,
@@ -44,16 +105,9 @@ public class PlayfabMonetizationExporter : MonoBehaviour
                 RechargeRate = 0,
                 RechargeMax = 0,
                 CurrencyCodeFull = code + " (" + name +")",
-                GameManagerClassMetadata = null,
             });
-
-            if (i == monetizationManager.currencies.Count - 1)
-            {
-                json += "]";
-            }
-
-            i++;
         }
+        var json = JsonMapper.ToJson(currencies);
         var path = EditorUtility.SaveFilePanel("Export Currencies", Application.dataPath, "CURRENCIES", "json");
         if (path.Length > 0)
             File.WriteAllText(path, json);
@@ -61,7 +115,7 @@ public class PlayfabMonetizationExporter : MonoBehaviour
 #endif
 
     [System.Serializable]
-    public struct PlayfabCurrency
+    public class PlayfabCurrency
     {
         public string CurrencyCode;     // GE
         public string DisplayName;      // Gem
@@ -69,6 +123,78 @@ public class PlayfabMonetizationExporter : MonoBehaviour
         public int RechargeRate;        // 0
         public int RechargeMax;         // 0
         public string CurrencyCodeFull; // GE (Gem)
-        public string GameManagerClassMetadata;     // null
+        public string GameManagerClassMetadata;
+    }
+
+    [System.Serializable]
+    public class PlayfabCatalog
+    {
+        public string CatalogVersion;
+        public List<CatalogItem> Catalog;
+        public List<DropTableItem> DropTables;
+    }
+
+    [System.Serializable]
+    public class CatalogItem
+    {
+        public string ItemId;
+        public string ItemClass;
+        public string CatalogVersion;
+        public string DisplayName;
+        public string Description;
+        public Dictionary<string, int> VirtualCurrencyPrices;
+        public Dictionary<string, int> RealCurrencyPrices;
+        public List<string> Tags;
+        public object CustomData;
+        public CatalogConsumable Consumable;
+        public CatalogContainer Container;
+        public CatalogBundle Bundle;
+        public bool CanBecomeCharacter;
+        public bool IsStackable;
+        public bool IsTradable;
+        public string ItemImageUrl;
+        public bool IsLimitedEdition;
+        public int InitialLimitedEditionCount;
+        public object ActivatedMembership;
+    }
+
+    [System.Serializable]
+    public class CatalogConsumable
+    {
+        public int? UsageCount;
+        public int? UsagePeriod;
+        public string UsagePeriodGroup;
+    }
+
+    [System.Serializable]
+    public class CatalogContainer
+    {
+        public string KeyItemId;
+        public List<string> ItemContents;
+        public List<string> ResultTableContents; // Drop tables
+        public Dictionary<string, int> VirtualCurrencyContents;
+    }
+
+    [System.Serializable]
+    public class CatalogBundle
+    {
+        public List<string> BundledItems;
+        public List<string> BundledResultTables; // Drop tables
+        public Dictionary<string, int> BundledVirtualCurrencies;
+    }
+
+    [System.Serializable]
+    public class DropTableItem
+    {
+        public string TableId;
+        public List<DropTableItemNode> Nodes;
+    }
+
+    [System.Serializable]
+    public class DropTableItemNode
+    {
+        public string ResultItemType = "ItemId";
+        public string ResultItem;
+        public int Weight;
     }
 }
