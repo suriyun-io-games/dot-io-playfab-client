8662b491ce2b9fc6147f8d395b196adb56e7ae7b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-19 22:42:43 +0700

    Add override component

---
+++ Scripts/PlayfabOverridePurchaseFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_PURCHASING && (UNITY_IOS || UNITY_ANDROID)
+using UnityEngine.Purchasing;
+#endif
+
+public class PlayfabOverridePurchaseFunctions : MonoBehaviour
+{
+    private void Awake()
+    {
+#if UNITY_PURCHASING && (UNITY_IOS || UNITY_ANDROID)
+        InGameProductData.OverrideBuyFunction = BuyFunction;
+        InGameProductData.OverrideBuyFunction = BuyWithCurrencyIdFunction;
+        MonetizationManager.OverrideSaveAdsReward = SaveAdsReward;
+        MonetizationManager.OverrideProcessPurchase = ProcessPurchase;
+#endif
+    }
+
+#if UNITY_PURCHASING && (UNITY_IOS || UNITY_ANDROID)
+    private static void BuyFunction(InGameProductData productData, System.Action<bool, string> callback)
+    {
+
+    }
+
+    private static void BuyWithCurrencyIdFunction(InGameProductData productData, System.Action<bool, string> callback)
+    {
+
+    }
+
+    private static void SaveAdsReward(AdsReward adsReward)
+    {
+
+    }
+
+    private static PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+#endif
+}
